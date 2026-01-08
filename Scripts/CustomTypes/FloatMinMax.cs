8b54d0fc470651437d9907d77bda7c6ec46fa3e4
Derek Sliman <lionext.studio@gmail.com>
2026-01-09 01:11:21 +0200

    Added min-max fields

---
+++ Scripts/CustomTypes/FloatMinMax.cs
@@ -0,0 +1,50 @@
+// Copyright (c) 2023 Derek Sliman
+// Licensed under the MIT License. See LICENSE.md for details.
+
+using System;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+using UnityRandom = UnityEngine.Random;
+
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
+namespace TinyUtilities.CustomTypes {
+#if ODIN_INSPECTOR
+    [InlineProperty]
+#endif
+    [Serializable]
+    public sealed class FloatMinMax
+#if UNITY_EDITOR && ODIN_VALIDATOR
+        : ISelfValidator
+#endif
+    {
+    #if ODIN_INSPECTOR
+        [field: HideLabel, SuffixLabel("min", true), HorizontalGroup]
+    #endif
+        [field: SerializeField]
+        public float min { get; private set; }
+        
+    #if ODIN_INSPECTOR
+        [field: HideLabel, SuffixLabel("max", true), HorizontalGroup]
+    #endif
+        [field: SerializeField]
+        public float max { get; private set; }
+        
+        [Pure]
+        public float GetRandom() => UnityRandom.Range(min, max);
+        
+        [Pure]
+        public float Clamp(float value) => Mathf.Clamp(value, min, max);
+        
+    #if UNITY_EDITOR && ODIN_VALIDATOR
+        
+        public void Validate(SelfValidationResult result) {
+            min = Mathf.Min(min, max);
+            max = Mathf.Max(min, max);
+        }
+        
+    #endif
+    }
+}
