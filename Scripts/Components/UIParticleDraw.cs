78aa0dc82bba463c69adbb112685d86bb95c7cd4
Derek Sliman <lionext.studio@gmail.com>
2026-08-18 01:39:58 +0300

    Added draft UI-Particle

---
+++ Scripts/Components/UIParticleDraw.cs
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 Derek Sliman
+// Licensed under the MIT License. See LICENSE.md for details.
+
+namespace TinyUtilities.Components {
+    public sealed class UIParticleDraw {
+        // TODO
+    }
+}
