759928959ebdd16a11d05447d8fd0fa86c214d28
Derek Sliman <lionext.studio@gmail.com>
2026-08-27 15:59:57 +0300

    Fixed unity object extension

--- Scripts/Extensions/UnityObjectExtension.cs
+++ Scripts/Extensions/UnityObjectExtension.cs
@@ -1,11 +1,11 @@
 // Copyright (c) 2023 Derek Sliman
 // Licensed under the MIT License. See LICENSE.md for details.
 
-using UnityEngine;
+using UnityObject = UnityEngine.Object;
 
 namespace TinyUtilities.Extensions {
-    public static class ObjectExtension {
-        public static void TrySetDirty<T>(this T obj) where T : Object {
+    public static class UnityObjectExtension {
+        public static void TrySetDirty<T>(this T obj) where T : UnityObject {
         #if UNITY_EDITOR
             if (obj != null) {
                 UnityEditor.EditorUtility.SetDirty(obj);
